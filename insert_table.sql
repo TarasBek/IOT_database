f554ef1591e8c0ea4b8c9338dc4b0a2b0ad57766
TarasBek <tarasbek2002@gmail.com>
2021-09-28 18:54:34 +0300

    adding files to  1 lab

---
+++ insert_table.sql
@@ -0,0 +1,42 @@
+-- store
+INSERT INTO `` (`id_stores`,`address`,`price`,`object_arrea`,`phone_number`,`price_dependence_id_price_dependence`,`product_id_product`) VALUES (2,'Lviv',123,123,123,1,1);
+INSERT INTO `` (`id_stores`,`address`,`price`,`object_arrea`,`phone_number`,`price_dependence_id_price_dependence`,`product_id_product`) VALUES (6,'Lviv',444,432,332,1,1);
+
+-- price_dependence
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (1,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (2,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (3,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (4,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (5,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (6,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (7,0,1);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (8,1,0);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (9,1,0);
+INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (10,0,1);
+
+--product
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (1,'Vaflia',12,'good vaflia',1,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (20,'Banana',13,'good stuff',2,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (21,'Bread',14,'good stuff',3,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (22,'Milk',31,'good stuff',4,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (23,'Butter',55,'good stuff',5,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (24,'Jam',44,'good stuff',6,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (25,'Shokolate',66,'good stuff',7,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (26,'Nuts',77,'good stuff',8,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (27,'Water',15,'good stuff',9,'12.02.2020');
+INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (28,'Water',4,'good stuff',10,'12.02.2020');
+);
+
+--characteristics
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (1,12,21,'Ukraine');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (2,21,12,'Ukraine');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (3,32,33,'Ukraine');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (4,12,124,'Poland');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (5,41,141,'Poland');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (6,51,111,'Poland');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (7,11,155,'Germany');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (8,4,142,'Germany');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (9,14,98,'Germany');
+INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (10,46,30,'Germany');
+
+
