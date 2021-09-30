d5447c12a510df097ded06b63d47a161f5007a9d
TarasBek <tarasbek2002@gmail.com>
2021-09-30 14:45:57 +0300

    adding improved files

--- insert_table.sql
+++ insert_table.sql
@@ -1,42 +1,60 @@
--- store
-INSERT INTO `` (`id_stores`,`address`,`price`,`object_arrea`,`phone_number`,`price_dependence_id_price_dependence`,`product_id_product`) VALUES (2,'Lviv',123,123,123,1,1);
-INSERT INTO `` (`id_stores`,`address`,`price`,`object_arrea`,`phone_number`,`price_dependence_id_price_dependence`,`product_id_product`) VALUES (6,'Lviv',444,432,332,1,1);
 
--- price_dependence
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (1,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (2,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (3,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (4,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (5,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (6,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (7,0,1);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (8,1,0);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (9,1,0);
-INSERT INTO `` (`id_price_dependence`,`fast_delivering_price`,`normal_delivering_price`) VALUES (10,0,1);
+INSERT INTO `` (`iduser`,`first_name`,`second_name`,`email`,`user_address_idstore_address`,`order_idorder`) VALUES (1,'Olena','Olena','olena20@gmail.com',1,1);
 
---product
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (1,'Vaflia',12,'good vaflia',1,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (20,'Banana',13,'good stuff',2,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (21,'Bread',14,'good stuff',3,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (22,'Milk',31,'good stuff',4,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (23,'Butter',55,'good stuff',5,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (24,'Jam',44,'good stuff',6,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (25,'Shokolate',66,'good stuff',7,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (26,'Nuts',77,'good stuff',8,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (27,'Water',15,'good stuff',9,'12.02.2020');
-INSERT INTO `` (`id_product`,`name`,`price`,`description`,`characteristics_id_characteristics`,`exp_date`) VALUES (28,'Water',4,'good stuff',10,'12.02.2020');
-);
 
 --characteristics
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (1,12,21,'Ukraine');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (2,21,12,'Ukraine');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (3,32,33,'Ukraine');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (4,12,124,'Poland');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (5,41,141,'Poland');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (6,51,111,'Poland');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (7,11,155,'Germany');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (8,4,142,'Germany');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (9,14,98,'Germany');
-INSERT INTO `` (`id_characteristics`,`nutritional_value`,`weight`,`country_of_manufacture`) VALUES (10,46,30,'Germany');
 
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (1,12,223);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (2,100,443);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (3,233,432);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (4,321,123);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (5,432,321);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (6,123,543);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (7,123,432);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (8,123,234);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (9,133,443);
+INSERT INTO `` (`idcharacteristics`,`weight`,`nutritions`) VALUES (10,452,645);
+
+--oreder
+
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (1,433,1);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (29,432,2);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (30,546,3);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (31,232,4);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (32,22,5);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (33,111,6);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (34,765,7);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (35,453,8);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (36,857,9);
+INSERT INTO `` (`idorder`,`price`,`product_idproduct`) VALUES (37,123,10);
+
+--product
+
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (1,'banana',23,1);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (2,'bread',14,2);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (3,'water',31,3);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (4,'milk',32,4);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (5,'mango',77,5);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (6,'potato',12,6);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (7,'chips',51,7);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (8,'cream',41,8);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (9,'biscuit',22,9);
+INSERT INTO `` (`idproduct`,`name`,`price`,`characteristics_idcharacteristics`) VALUES (10,'cake',333,10);
+
+--store
+
+INSERT INTO `` (`idstore`,`user_iduser`,`user_user_address_idstore_address`,`store_address_idstore_address`) VALUES (11,1,1,1);
+
+--store_address
+
+INSERT INTO `` (`idstore_address`,`street`,`city`) VALUES (1,'Stryiska','Lviv');
+
+--user
+
+
+INSERT INTO `` (`iduser`,`first_name`,`second_name`,`email`,`user_address_idstore_address`,`order_idorder`) VALUES (1,'Olena','Olena','olena20@gmail.com',1,1);
+
+--user_adderss
+
+INSERT INTO `` (`idstore_address`,`city`,`street`,`house_number`,`flat_number`) VALUES (1,'Lviv','Soborna',4,23);
 
